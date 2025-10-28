2e36c9d51015a263a291468f8697c90aeffab651
joao pedro <joao.pedrozg@hotmail.com>
2025-10-28 17:46:28 -0300

    adicionado contextapi

---
+++ src/contexts/CartContext.tsx
@@ -0,0 +1,44 @@
+
+
+import { Children, createContext, ReactNode, useState } from "react";
+import { ProductProps } from "../pages/home";
+
+interface CartContextData {
+    cart: CartProps[];
+    cartAmount: number;
+}
+
+interface CartProps {
+    id: number;
+    title: string;
+    description: string;
+    price: number;
+    cover : string;
+    amount : number;
+    total : number;
+}
+
+interface CartProviderProps{
+    children: ReactNode;
+}
+
+export const CartContext = createContext({} as CartContextData)
+   
+
+    function CartProvider({ children }: CartProviderProps){
+        const [ cart, setCart] = useState<CartProps[]>([]);
+
+        function addItem(newItem : ProductProps){
+
+        }
+        return (
+            <CartContext.Provider value={{
+                cart,
+                cartAmount:cart.length
+                }}>
+                { children }
+            </CartContext.Provider>
+        )
+    }
+
+export default CartProvider;
